785515884a7574cceff89b115bd3282044031610
nono92vn <ps1505002@gmail.com>
2024-01-30 21:46:35 +0900

    remove action-runner

--- actions-runner/_work/_actions/docker/build-push-action/v5/src/state-helper.ts
+++
@@ -1,7 +0,0 @@
-import * as core from '@actions/core';
-
-export const tmpDir = process.env['STATE_tmpDir'] || '';
-
-export function setTmpDir(tmpDir: string) {
-  core.saveState('tmpDir', tmpDir);
-}
